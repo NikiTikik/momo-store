b88284397a556a65968c5f278389b1c8f10b4411
std-015-37 std-015-37 <std-015-37@praktikum-services.ru>
2023-08-15 19:55:19 +0000

    Update main.tf

--- infra/momo-store-cluster/main.tf
+++ infra/momo-store-cluster/main.tf
@@ -170,7 +170,7 @@ resource "yandex_kubernetes_node_group" "my_node_group" {
 }
 
 output "cluster_external_v4_endpoint" {
-  value = yandex_kubernetes_cluster.zonal_cluster_resource_name.master.0.external_v4_endpoint
+  value = yandex_kubernetes_cluster.k8s-momo.0.external_v4_endpoint
 }
 
 output "my_node_group_status" {
