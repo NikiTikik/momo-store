09bee0a065e0e306799b050362b2261f3e0b7e18
std-015-37 std-015-37 <std-015-37@praktikum-services.ru>
2023-08-21 21:03:54 +0000

    Update main.tf

--- infra/momo-store-cluster/main.tf
+++ infra/momo-store-cluster/main.tf
@@ -31,12 +31,25 @@ resource "yandex_vpc_subnet" "mysubnet" {
   network_id     = yandex_vpc_network.mynet.id
 }
 
+resource "yandex_iam_service_account" "myaccount" {
+  name        = var.sa_name
+  description = "K8S zonal service account"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "k8s-clusters-agent" {
+  # Сервисному аккаунту назначается роль "k8s.clusters.agent".
+  folder_id = var.folder_id
+  role      = "k8s.clusters.agent"
+  member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
+}
+
 resource "yandex_resourcemanager_folder_iam_member" "vpc-public-admin" {
   # Сервисному аккаунту назначается роль "vpc.publicAdmin".
   folder_id = var.folder_id
   role      = "vpc.publicAdmin"
   member    = "serviceAccount:${yandex_iam_service_account.myaccount.id}"
 }
+
 
 resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
   # Сервисному аккаунту назначается роль "container-registry.images.puller".
