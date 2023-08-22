d21bcef88c139a25d34ffad46afc1beb80a44cc1
std-015-37 std-015-37 <std-015-37@praktikum-services.ru>
2023-08-22 20:36:36 +0000

    Update main.tf

--- infra/momo-store-cluster/modules/tf-yc-network/main.tf
+++ infra/momo-store-cluster/modules/tf-yc-network/main.tf
@@ -1,10 +1,3 @@
-# data "yandex_vpc_network" "default" {
-#   name = "default"
-# }
-
-# data "yandex_vpc_subnet" "default" {
-#   name = "${data.yandex_vpc_network.default.name}-${var.zone}"
-# }
 resource "yandex_vpc_network" "mynet" {
   name = "mynet"
 }
