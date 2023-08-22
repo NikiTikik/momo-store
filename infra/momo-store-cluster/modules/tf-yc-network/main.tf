66d6b71445835abde4f28518aa86d7b7f5fb55e1
std-015-37 std-015-37 <std-015-37@praktikum-services.ru>
2023-08-22 18:57:05 +0000

    Update main.tf

--- infra/momo-store-cluster/modules/tf-yc-network/main.tf
+++ infra/momo-store-cluster/modules/tf-yc-network/main.tf
@@ -5,5 +5,5 @@ data "yandex_vpc_network" "mynet" {
 data "yandex_vpc_subnet" "mysubnet" {
   v4_cidr_blocks = ["10.1.0.0/16"]
   zone           = var.zone
-  network_id     = yandex_vpc_network.mynet
+  network_id     = "${data.yandex_vpc_network.mynet.id}"
 }
