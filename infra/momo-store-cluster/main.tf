b34edac977d24d2c8a5ca8769d08767ee8f42d6d
std-015-37 std-015-37 <std-015-37@praktikum-services.ru>
2023-08-22 18:44:09 +0000

    Update main.tf

--- infra/momo-store-cluster/main.tf
+++ infra/momo-store-cluster/main.tf
@@ -10,7 +10,11 @@ module "tf-yc-cluster" {
   source = "./modules/tf-yc-cluster"
   k8s_version = "1.25"
   zone = "ru-central1-a"
-  k8s_subnet_id = module.tf-yc-network.yandex_vpc_network.subnet_id
-  k8s_network_id = module.tf-yc-network.yandex_vpc_network.network_id
-  k8s_yandex_iam_service_account_id = module.tf-yc-iam.yandex_iam_service_account_id
+  k8s_subnet_id = "ekk" #module.tf-yc-network.yandex_vpc_network.subnet_id
+  k8s_network_id = "ekk" #module.tf-yc-network.yandex_vpc_network.network_id
+  k8s_yandex_iam_service_account_id = "ekk" #module.tf-yc-iam.yandex_iam_service_account_id
 } 
+
+output "k8s_subnet_id" {
+  value = module.tf-yc-network.yandex_vpc_network
+}
